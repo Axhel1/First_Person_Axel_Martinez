cd512e729c05473a65df7e5d174a435364b0946a
Axhel1 <axelcomoaxelm888@gmail.com>
2024-10-28 21:24:03 +0100

    PrimeraPersona

--- Assets/Script/Personaje.cs
+++ Assets/Script/Personaje.cs
@@ -22,10 +22,12 @@ public class Personaje : MonoBehaviour
 
 
     CharacterController Control;
+     [SerializeField]Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         Control = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();    
     }
 
     // Update is called once per frame
@@ -33,18 +35,24 @@ public class Personaje : MonoBehaviour
     {
         MoverYrotar();
 
-        
+
 
         if (moverPlayer.magnitude >= 0.1f)
         {
-
+            anim.SetBool("IsWalk",true);
             targetAngulo = Mathf.Atan2(moverPlayer.x, moverPlayer.z) * Mathf.Rad2Deg;
             angulo = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngulo, ref smoothVelocity, smoothTime);
-            
+
             transform.rotation = Quaternion.Euler(0f, angulo, 0f);
-           
 
 
+            Control.Move(moverPlayer * playerSpeed * Time.deltaTime);
+
+
+
+        }
+        else {
+            anim.SetBool("IsWalk", false);
         }
 
 
@@ -55,7 +63,12 @@ public class Personaje : MonoBehaviour
         horizontalMove = Input.GetAxis("Horizontal");
         verticalMove = Input.GetAxis("Vertical");
         moverPlayer = new Vector3(horizontalMove, 0, verticalMove).normalized;
-        //float anguloRotacion= 
-        Control.Move(moverPlayer * playerSpeed * Time.deltaTime);
+      
+    }
+
+
+    private void IsGround() {
+
+        //Physics.CheckSphere();
     }
 }
